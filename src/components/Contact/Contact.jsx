354d2986ed78dffcc56c4d65427597264828297b
Marina Ilyushina <m_ilyushina@hotmail.com>
2024-05-13 20:55:12 +0200

    contact styles responsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,9 +13,9 @@ export default function Contact({className, title, subtitle, image, logo, addres
   return (
     <div className={cl(className, styles.contact)}>
         <div className={cl(className, styles.contactLeft)}>
-            <Title type="medium" 
-            className={cl(className, styles.contactTitle)}>{title}</Title>
-            <p className={cl(className, styles.contactText)}> {subtitle}</p>
+            <h3 
+            className={cl(className, styles.contactLeftTitle)}>{title}</h3>
+            <p className={cl(className, styles.contactLeftText)}> {subtitle}</p>
             <Form />
         </div>
         <div className={cl(className, styles.contactRight)}>
@@ -24,14 +24,14 @@ export default function Contact({className, title, subtitle, image, logo, addres
             alt="contact"
             width={350}
             height={350}
-            className={cl(className, styles.contactImage)}
+            className={cl(className, styles.contactRightImage)}
             />
             <Image
                 src={urlFor(logo).url()}
                 alt="logo"
                 width={140}
                 height={70}
-                className={cl(className, styles.contactLogo)}
+                className={cl(className, styles.contactRightLogo)}
             />
             <div className={cl(className, styles.contactRightData)}>
                 <div className={cl(className, styles.contactRightDataText)}>
